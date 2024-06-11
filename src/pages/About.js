83951a9a9e3c725c25c06fb99e7f2d16a9a6a757
Michael Crabb <Michael.M.Crabb-1@ou.edu>
2024-06-10 20:59:22 -0500

    Final redo because autograder failed

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,7 +21,7 @@ export default class About extends Component {
             <div className="centered">
               <div className="name_title">Michael Crabb</div>
               <div className="brief_description">
-                Hello!! My name is Michael Crabb and I am a student at the University of Oklahoma in Norman, Oklahoma. I
+                Hello! My name is Michael Crabb and I am a student at the University of Oklahoma in Norman, Oklahoma. I
                 study computer science and mathematics and feel so privileged to be here with you all. My interests vary,
                 I am insanely captivated by the Earth, I have many plans of traveling the world and seeing how it can
                 surprise me next and I enjoy playing video games with my friends.
